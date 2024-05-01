1124fcf7d0d6f343aeeedfe9af637955bca71302
roshani <rsg.bhusal@gmail.com>
2024-05-01 09:05:01 +0545

    aboutus page

---
+++ src/components/footer/page.js
@@ -0,0 +1,86 @@
+import React from "react";
+import Image from "next/image";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import XIcon from "@mui/icons-material/X";
+
+function Footer() {
+  return (
+    <>
+      <div className="w-full bg-sm-lightblue py-10">
+        <div className="flex justify-around px-14 gap-12">
+          <div>
+            <Image src="/logo.svg" width={200} height={100} alt="img" />
+            <p className="text-white text-left">
+              Illuminating Minds, Inspiring Innovation,<br></br> and Shaping
+              Global Leaders
+            </p>
+          </div>
+
+          <div className="flex-initial md:w-2/12 font-light text-left">
+            <div className="mt-4 text-md grid grid-rows-1 gap-2 text-white">
+              <div className="mb-1">
+                <a href="#">Home</a>
+              </div>
+              <div className="mb-1">
+                <a href="#">Academics</a>
+              </div>
+              <div className="mb-1">
+                <a href="#">Admission</a>
+              </div>
+              <div>
+                <a href="#">News/Calendar</a>
+              </div>
+            </div>
+          </div>
+          <div className="flex-initial md:w-2/12 font-light text-left">
+            <div className="mt-4 text-md grid grid-rows-1 gap-2 text-white">
+              <div className="mb-1">
+                <a href="#">Contact</a>
+              </div>
+              <div className="mb-1">
+                <a href="#">Nepal</a>
+              </div>
+              <div className="mb-1">
+                <a href="#">01111111</a>
+              </div>
+              <div>
+                <a href="#">dynamicschool@gmail.com</a>
+              </div>
+            </div>
+          </div>
+          <div className="flex-initial md:w-4/12 text-left">
+            <h1 className="text-md text-white mb-4">social media link</h1>
+
+            <div className="mt-4 flex flex-row gap-6 ">
+              <a>
+                <FacebookIcon className="text-white" />
+              </a>
+              <a>
+                <XIcon className="text-white" />
+              </a>
+              <a>
+                <InstagramIcon className="text-white" />
+              </a>
+            </div>
+            <p className="text-md text-white mb-2">Term and condition</p>
+          </div>
+        </div>
+
+        
+      </div>
+      <div className="bg-sm-blue">
+          <div className="w-11/12 mx-auto  text-white text-xs font-light ">
+            <p className="text-center text-base py-4">
+              &copy; 2023 - {new Date().getFullYear()} Travel the world pvt.
+              Ltd.All rights reserved.
+            </p>
+
+            <div className="clear-both"></div>
+          </div>
+        </div>
+    </>
+  );
+}
+
+export default Footer;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        sm:{
+          green:'#5BCE60',
+          darkblue:'#1B2835',
+          lightblue:'#3498DB',
+          blue:'#1d5478',
+          blue700:'#256c9b',
+          green100:'#ccf0ce'
+        }
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
